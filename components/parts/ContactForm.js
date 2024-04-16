adc1db24b72f258b3c95e43ebacd689c4ee8be6d
ayu <ayuabc4528@gmail.com>
2024-04-16 18:10:45 +0900

    Change contact form

--- components/parts/ContactForm.js
+++ components/parts/ContactForm.js
@@ -5,19 +5,27 @@ import axios from "axios"
 const ContactForm = () => {
 	const router = useRouter()
 
-	const [name, setName] = useState('')
-	const [email, setEmail] = useState('')
-	const [content, setContent] = useState('')
+	const [formData, setFormData] = useState({
+		company: "",
+		name: "",
+		furigana: "",
+		tel: "",
+		email: "",
+		content: ""
+	})
+	
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value
+		})
+	}
 
 	const handleSubmit = async (e) => {
 		e.preventDefault()
 		try {
 			await axios.post(`${process.env.NEXT_PUBLIC_MICROCMS_CONTACT_API_URL}`,
-				{
-					name: name,
-					email: email,
-					content: content
-				},
+				formData,
 				{
 					headers: {
 						"Content-Type": "application/json",
@@ -37,16 +45,28 @@ const ContactForm = () => {
 				<table>
 					<tbody>
 						<tr>
+							<th><span className="label ninni">任意</span>貴社名</th>
+							<td><input type="text" placeholder="〇〇株式会社" name="company" value={formData.company} onChange={handleChange} /></td>
+						</tr>
+						<tr>
 							<th><span className="label hissu">必須</span>お名前</th>
-							<td><input type="text" placeholder="田中太郎" name="name" value={name} required onChange={(e) => setName(e.target.value)} /></td>
+							<td><input type="text" placeholder="田中太郎" name="name" value={formData.name} required onChange={handleChange} /></td>
+						</tr>
+						<tr>
+							<th><span className="label ninni">任意</span>ふりがな</th>
+							<td><input type="text" placeholder="たなかたろう" name="furigana" value={formData.furigana} onChange={handleChange} /></td>
+						</tr>
+						<tr>
+							<th><span className="label hissu">必須</span>電話番号</th>
+							<td><input type="tel" placeholder="00012345678" name="tel" value={formData.tel} required onChange={handleChange} /></td>
 						</tr>
 						<tr>
 							<th><span className="label hissu">必須</span>メールアドレス</th>
-							<td><input type="email" placeholder="example@test.co.jp" name="email" value={email} required onChange={(e) => setEmail(e.target.value)} /></td>
+							<td><input type="email" placeholder="example@test.co.jp" name="email" value={formData.email} required onChange={handleChange} /></td>
 						</tr>
 						<tr>
 							<th><span className="label ninni">任意</span>お問い合わせ内容</th>
-							<td><textarea value={content} name="content" onChange={(e) => setContent(e.target.value)} /></td>
+							<td><textarea value={formData.content} name="content" onChange={handleChange} /></td>
 						</tr>
 					</tbody>
 				</table>
